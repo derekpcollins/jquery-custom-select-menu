13350f8811fb31b48b57039484d3f218963cc0c7
Derek P. Collins <derekpcollins@gmail.com>
2013-02-14 13:07:56 -0800

    Removed default Jasmine files

--- spec/javascripts/custom-select-menu-spec.js
+++ spec/javascripts/custom-select-menu-spec.js
@@ -1,32 +1,5 @@
 describe('Custom Select Menu', function () {
 
-  beforeEach( function () {
-    $('<select name="select-menu-1"></select>').appendTo('body');
-    $('<option>Choose one...</option>').appendTo('select');
-    for (var i=1; i < 4; i++) {
-      $('<option value="' + i + '">' + i + '</option>').appendTo('select');
-    };
-
-    this.addMatchers({
-      toBeHidden: function() {
-        var isHidden = true;
-
-        this.actual.find('select').each(function () {
-          isHidden = $(this).is(':visible');
-          if (!isHidden) {
-            return;
-          }
-        });
-
-        return isHidden;
-      }
-    });
-  });
-
-  afterEach( function () {
-    $('select').remove();
-  });
-
   it('hides the original select element', function () {
     var originalSelect = $('select');
     originalSelect.customSelectMenu();
--- spec/javascripts/helpers/SpecHelper.js
+++ spec/javascripts/helpers/SpecHelper.js
@@ -1,9 +1,30 @@
 beforeEach(function() {
+  
+  $('<select name="select-menu-1"></select>').appendTo('body');
+  $('<option>Choose one...</option>').appendTo('select');
+  for (var i=1; i < 4; i++) {
+    $('<option value="' + i + '">' + i + '</option>').appendTo('select');
+  };
+
   this.addMatchers({
-    toBePlaying: function(expectedSong) {
-      var player = this.actual;
-      return player.currentlyPlayingSong === expectedSong && 
-             player.isPlaying;
+    toBeHidden: function() {
+      var isHidden = true;
+
+      this.actual.find('select').each(function () {
+        isHidden = $(this).is(':visible');
+        if (!isHidden) {
+          return;
+        }
+      });
+
+      return isHidden;
     }
   });
+
+});
+
+afterEach( function () {
+
+  //$('select').remove();
+  
 });
