370df4dc8e1ffbf17a86e09ba2099dd5ef9c776e
Derek P. Collins <derekpcollins@gmail.com>
2013-02-14 14:24:45 -0800

    Added new tests to spec

--- spec/javascripts/custom-select-menu-spec.js
+++ spec/javascripts/custom-select-menu-spec.js
@@ -12,11 +12,36 @@ describe('Custom Select Menu', function () {
       expect(hiddenInput).toBeHidden();
     });
 
+    it('has the same name as the original select element', function () {
+      var originalSelectName = $('select').attr('name'),
+          hiddenInputName    = $('input:hidden').attr('name');
+
+      expect(hiddenInputName).toEqual(originalSelectName);
+    });
+
   });
 
-  it('creates a wrapper div', function () {
-    var wrapperDiv = $('div');
-    expect(wrapperDiv).toExist();
+  describe('A wrapper div', function () {
+
+    it('is created', function () {
+      var wrapperDiv = $('div');
+      expect(wrapperDiv).toExist();
+    });
+
+    it('has a default class name of custom-select-menu', function() {
+      var wrapperDiv = $('div.custom-select-menu');
+      expect(wrapperDiv).toExist();
+    });
+
+    it('can accept an optional class name', function() {
+      $('select').customSelectMenu({
+        customMenuClassName : 'foo-bar'
+      });
+
+      var wrapperDiv = $('div.foo-bar');
+      expect(wrapperDiv).toExist();
+    });
+
   });
 
   it('hides the original select element', function () {
