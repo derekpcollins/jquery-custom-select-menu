c84307fee80e85438616e0f6a88b52b98e848e4b
Derek P. Collins <derekpcollins@gmail.com>
2013-02-27 10:26:55 -0800

    Updated/finished Jasmine tests

--- spec/javascripts/custom-select-menu-spec.js
+++ spec/javascripts/custom-select-menu-spec.js
@@ -46,11 +46,21 @@ describe('Custom Select Menu', function() {
       expect( labelClass ).toContain( 'opened' );
     });
 
+    it('has class of opened removed on second click', function() {
+      // First click adds class
+      $('.custom-select-menu label').click();
+      var labelClass = $('.custom-select-menu label').attr( 'class' );
+      expect( labelClass ).toContain( 'opened' );
+      // Second click removes class
+      $('.custom-select-menu label').click();
+      var labelClass = $('.custom-select-menu label').attr( 'class' );
+      expect( labelClass ).toContain( '' );
+    });
+
     it('gets a class of selection made when an option is selected', function() {
       var listItem = $('.custom-select-menu li[data-option-value="red"]');
       listItem.click();
       var labelClass = $('.custom-select-menu label').attr( 'class' );
-      console.log( labelClass );
       expect( labelClass ).toContain( 'selection-made' );
     });
 
@@ -58,13 +68,6 @@ describe('Custom Select Menu', function() {
 
   // UNORDERED LIST
   describe('Unordered list', function() {
-
-    /*it('has the same number of items as original options', function() {
-      var originalOptions = $('select option').length,
-          newListItems = $('.custom-select-menu li').length;
-
-      expect( newListItems ).toEqual( originalOptions );
-    });*/
 
     it('has items with the same text as the original options', function() {
       var listItemTexts = [],
@@ -113,6 +116,15 @@ describe('Custom Select Menu', function() {
       expect( $('.custom-select-menu ul') ).toBeVisible();
     });
 
+    it('is hidden on click if the menu is already being shown', function() {
+      // First click opens it
+      $('.custom-select-menu label').click();
+      expect( $('.custom-select-menu ul') ).toBeVisible();
+      // Second click closes it
+      $('.custom-select-menu label').click();
+      expect( $('.custom-select-menu ul') ).toBeHidden();
+    });
+
     it('is hidden if there is a click outside the menu', function() {
       // Open the menu first
       $('.custom-select-menu label').click();
@@ -133,6 +145,21 @@ describe('Custom Select Menu', function() {
       });
     
       $('body').trigger(e);
+
+      expect( $('.custom-select-menu ul') ).toBeHidden();
+    });
+
+    it('is hidden if the return/enter key is pressed', function() {
+      // Open the menu first
+      $('.custom-select-menu label').click();
+      expect( $('.custom-select-menu ul') ).toBeVisible();
+
+      // Simulate enter/return being pressed
+      var e = $.Event('keyup', {
+        keyCode: 13
+      });
+
+      $('.custom-select-menu').trigger(e);
 
       expect( $('.custom-select-menu ul') ).toBeHidden();
     });
@@ -172,17 +199,38 @@ describe('Custom Select Menu', function() {
 
   });
 
-  // LIST ITEMS
-  describe('List items', function() {
+  // LIST ITEM
+  describe('List item', function() {
 
     it('gets a class of selected when clicked', function() {
       var listItem = $('.custom-select-menu li[data-option-value="red"]');
       listItem.click();
 
-      expect( listItem.attr( 'class' ) ).toEqual( 'selected' );
+      expect( listItem ).toHaveClass( 'selected' );
     });
 
-    it('pass data-option-value to the hidden input when clicked', function() {
+    it('gets a class of selected when enter/return is pressed', function() {
+      // First simulate the down arrow being pressed to select the next option
+      var originalSelectedItem = $('.custom-select-menu li.selected');
+      var nextItem = originalSelectedItem.next();
+
+      var e = $.Event('keyup', {
+        keyCode: 40
+      });
+    
+      $('.custom-select-menu').trigger(e);
+      
+      // Simulate enter/return being pressed
+      var e2 = $.Event('keyup', {
+        keyCode: 13
+      });
+
+      $('custom-select-menu').trigger(e2);
+
+      expect( nextItem ).toHaveClass( 'selected' );
+    });
+
+    it('passes data-option-value to the hidden input when clicked', function() {
       var listItem = $('.custom-select-menu li[data-option-value="red"]');
       listItem.click();
       var inputValue = $('input:hidden').val();
@@ -190,14 +238,92 @@ describe('Custom Select Menu', function() {
       expect( inputValue ).toEqual( listItem.attr( 'data-option-value' ) );
     });
 
-  });
+    it('passes data-option-value to the hidden input when enter/return is pressed', function() {
+      // First simulate the down arrow being pressed to select the next option
+      var originalSelectedItem = $('.custom-select-menu li.selected');
+      var nextItem = originalSelectedItem.next();
 
-  /*
-   * TODO:
-   * Test that arrow keys select the li's
-   * Test for selection on return/enter
-   * Test that menu closes when selection is made
-   */
+      var e = $.Event('keyup', {
+        keyCode: 40
+      });
+    
+      $('.custom-select-menu').trigger(e);
+
+      // Simulate enter/return being pressed
+      var e2 = $.Event('keyup', {
+        keyCode: 13
+      });
+
+      $('.custom-select-menu').trigger(e2);
+
+      var inputValue = $('input:hidden').val();
+
+      expect( inputValue ).toEqual( nextItem.attr( 'data-option-value' ) );
+    });
+
+    it('selects previous item when the up arrow key is pressed', function() {
+      // In our test html, the first item is going to be selected, so for the purpose
+      // of this test, let's select a different item:
+      $('.custom-select-menu li.selected').removeClass( 'selected' );
+      $('.custom-select-menu li[data-option-value="orange"]').addClass( 'selected' );
+      
+      var originalSelectedItem = $('.custom-select-menu li.selected'),
+          prevItem             = originalSelectedItem.prev();
+
+      var e = $.Event('keyup', {
+        keyCode: 38
+      });
+    
+      $('.custom-select-menu').trigger(e);
+      expect( originalSelectedItem ).toHaveClass( '' );
+      expect( prevItem ).toHaveClass( 'selected' );
+      
+    });
+
+    it('selects next item when the down arrow key is pressed', function() {
+      var originalSelectedItem = $('.custom-select-menu li.selected');
+      var nextItem = originalSelectedItem.next();
+
+      var e = $.Event('keyup', {
+        keyCode: 40
+      });
+    
+      $('.custom-select-menu').trigger(e);
+      expect( originalSelectedItem ).toHaveClass( '' );
+      expect( nextItem ).toHaveClass( 'selected' );
+    });
+
+    it('selects the last item when the up arrow is pressed and the first item is selected', function() {
+      var originalSelectedItem = $('.custom-select-menu li.selected'),
+          lastItem             = $('.custom-select-menu li').last();
+
+      var e = $.Event('keyup', {
+        keyCode: 38
+      });
+
+      $('.custom-select-menu').trigger(e);
+      expect( originalSelectedItem ).toHaveClass( '' );
+      expect( lastItem ).toHaveClass( 'selected' );
+    });
+
+    it('selects the first item when the down arrow is pressed and the last item is selected', function() {
+      // Select the last item for this test
+      $('.custom-select-menu li.selected').removeClass( 'selected' );
+      $('.custom-select-menu li:last').addClass( 'selected' );
+
+      var originalSelectedItem = $('.custom-select-menu li.selected'),
+          firstItem            = $('.custom-select-menu li').first();
+
+      var e = $.Event('keyup', {
+        keyCode: 40
+      });
+
+      $('.custom-select-menu').trigger(e);
+      expect( originalSelectedItem ).toHaveClass( '' );
+      expect( firstItem ).toHaveClass( 'selected' );
+    });
+
+  });
 
 });
 
@@ -233,7 +359,6 @@ describe('Custom Select Menu Options', function() {
     var listItem = $('.my-menu li[data-option-value="red"]');
     listItem.click();
     var labelClass = $('.my-menu label').attr( 'class' );
-    console.log( labelClass );
     expect( labelClass ).toContain( 'has-been-selected' );
   });
 });
